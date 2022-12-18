5f03eba31095486e8f430650d6d0988f2f39a4cf
Daniel Eichinger <daniel1.eichinger@st.oth-regensburg.de>
2022-12-18 18:04:37 +0100

    Suspend default task

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -203,6 +203,7 @@ void StartDefaultTask(void *argument)
   /* init code for USB_DEVICE */
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN StartDefaultTask */
+  osThreadSuspend(defaultTaskHandle)
   /* Infinite loop */
   for(;;)
   {
